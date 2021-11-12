fcca46bf7201d2760f7e43e89acc3e9ea2aa0069
luffy <luffy@LUFFYD>
2021-11-12 03:30:40 -0500

    Agregar archivos de proyecto.

---
+++ Business/UniversidadBusiness.cs
@@ -0,0 +1,22 @@
+﻿using Data;
+using Entity;
+using NLog;
+using System;
+using System.Collections.Generic;
+
+namespace Business
+{
+    public class UniversidadBusiness
+    {
+        private readonly UniversidadData universidadData = new UniversidadData();
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public List<UniversidadEntity> LIS_UniversidadBusiness(string CODALU)
+        {
+            return universidadData.LIS_UniversidadData(CODALU);
+
+        }
+        
+        
+    }
+}
